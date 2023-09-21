0e9f4adf5aa14fc9d34bd89ae18dcc5dda1587d9
hisamuddin01 <hisamuddin357545@gmail.com>
2023-09-21 20:11:23 +0530

    menu section added

--- app.js
+++ app.js
@@ -1,3 +1,69 @@
+menu = [
+    {
+        id : 1,
+        title : "chocolate cake",
+        img : "./images/menu-item-01.jpg",
+        price : 5,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 2,
+        title : "classy panecake",
+        img : "./images/menu-item-02.jpg",
+        price : 8,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 3,
+        title : "Tall classy bread",
+        img : "./images/menu-item-03.jpg",
+        price : 9,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 4,
+        title : "blueberry cheesecake",
+        img : "./images/menu-item-04.jpg",
+        price : 2,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 5,
+        title : "classy cupcake",
+        img : "./images/menu-item-05.jpg",
+        price : 3,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 6,
+        title : "classy cupcake",
+        img : "./images/menu-item-03.jpg",
+        price : 3,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 7,
+        title : "classy cupcake",
+        img : "./images/menu-item-01.jpg",
+        price : 3,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 8,
+        title : "classy cupcake",
+        img : "./images/menu-item-05.jpg",
+        price : 3,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    },
+    {
+        id : 8,
+        title : "classy cupcake",
+        img : "./images/menu-item-04.jpg",
+        price : 3,
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, tempora?"
+    }
+]
+
 // ===========Nav Toggler ==========
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
@@ -22,4 +88,23 @@ window.addEventListener("scroll", ()=> {
     }else{
         navbar.classList.remove("fixed-nav")
     }
+});
+// parent container
+const menuItems = document.querySelector(".menu-items");
+window.addEventListener("DOMContentLoaded", function(){
+    let displayMenu = menu.map(function(item){
+        // console.log(item);
+        return `<div class="menu-item">
+        <img src=${item.img}>
+        <h5 class="price">$${item.price}</h5>
+        <div class="item-info">
+            <h4>${item.title}</h4>
+            <p>${item.desc}</p>
+            <a href="#" class="item-link">Make a reservation</a>
+        </div>
+    </div>`
+    });
+    displayMenu = displayMenu.join('');
+    console.log(displayMenu)
+    menuItems.innerHTML = displayMenu;
 });
